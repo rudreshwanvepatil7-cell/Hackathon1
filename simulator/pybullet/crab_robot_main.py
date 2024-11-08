06cd40affbc57d356cdd1b2533850ace40a0319b
Emily Sturman <sturman@utexas.edu>
2024-11-08 17:41:53 -0600

    full crab urdf

--- simulator/pybullet/crab_robot_main.py
+++ simulator/pybullet/crab_robot_main.py
@@ -26,42 +26,43 @@ def get_sensor_data_from_pybullet(robot):
     joint_pos, joint_vel = np.zeros(12), np.zeros(12)
 
     imu_frame_quat = np.array(pb.getLinkState(robot, Go2LinkIdx.imu, 1, 1)[1])
-    
-    # Front left 
+
+    # Front left
     joint_pos[0] = pb.getJointState(robot, Go2JointIdx.FL_hip_joint)[0]
     joint_pos[1] = pb.getJointState(robot, Go2JointIdx.FL_thigh_joint)[0]
     joint_pos[2] = pb.getJointState(robot, Go2JointIdx.FL_calf_joint)[0]
-    # Front right 
+    # Front right
     joint_pos[3] = pb.getJointState(robot, Go2JointIdx.FR_hip_joint)[0]
     joint_pos[4] = pb.getJointState(robot, Go2JointIdx.FR_thigh_joint)[0]
     joint_pos[5] = pb.getJointState(robot, Go2JointIdx.FR_calf_joint)[0]
-    # Rear left 
+    # Rear left
     joint_pos[6] = pb.getJointState(robot, Go2JointIdx.RL_hip_joint)[0]
     joint_pos[7] = pb.getJointState(robot, Go2JointIdx.RL_thigh_joint)[0]
     joint_pos[8] = pb.getJointState(robot, Go2JointIdx.RL_calf_joint)[0]
-    # Rear right 
+    # Rear right
     joint_pos[9] = pb.getJointState(robot, Go2JointIdx.RR_hip_joint)[0]
     joint_pos[10] = pb.getJointState(robot, Go2JointIdx.RR_thigh_joint)[0]
     joint_pos[11] = pb.getJointState(robot, Go2JointIdx.RR_calf_joint)[0]
 
     imu_ang_vel = np.array(pb.getLinkState(robot, Go2LinkIdx.imu, 1, 1)[7])
 
-    imu_dvel = pybullet_util.simulate_dVel_data(robot, Go2LinkIdx.imu,
-                                                previous_torso_velocity)
+    imu_dvel = pybullet_util.simulate_dVel_data(
+        robot, Go2LinkIdx.imu, previous_torso_velocity
+    )
 
-    # Front left 
+    # Front left
     joint_vel[0] = pb.getJointState(robot, Go2JointIdx.FL_hip_joint)[1]
     joint_vel[1] = pb.getJointState(robot, Go2JointIdx.FL_thigh_joint)[1]
     joint_vel[2] = pb.getJointState(robot, Go2JointIdx.FL_calf_joint)[1]
-    # Front right 
+    # Front right
     joint_vel[3] = pb.getJointState(robot, Go2JointIdx.FR_hip_joint)[1]
     joint_vel[4] = pb.getJointState(robot, Go2JointIdx.FR_thigh_joint)[1]
     joint_vel[5] = pb.getJointState(robot, Go2JointIdx.FR_calf_joint)[1]
-    # Rear left 
+    # Rear left
     joint_vel[6] = pb.getJointState(robot, Go2JointIdx.RL_hip_joint)[1]
     joint_vel[7] = pb.getJointState(robot, Go2JointIdx.RL_thigh_joint)[1]
     joint_vel[8] = pb.getJointState(robot, Go2JointIdx.RL_calf_joint)[1]
-    # Rear right 
+    # Rear right
     joint_vel[9] = pb.getJointState(robot, Go2JointIdx.RR_hip_joint)[1]
     joint_vel[10] = pb.getJointState(robot, Go2JointIdx.RR_thigh_joint)[1]
     joint_vel[11] = pb.getJointState(robot, Go2JointIdx.RR_calf_joint)[1]
@@ -92,95 +93,102 @@ def get_sensor_data_from_pybullet(robot):
         RR_normal_force += contact[9]
 
     # Determine foot contact states based on the z-coordinate of the foot link
-    b_FL_foot_contact = (True if pb.getLinkState(robot, Go2LinkIdx.FL_foot, 1, 1)[0][2] <= 0.05 else False)
-    b_FR_foot_contact = (True if pb.getLinkState(robot, Go2LinkIdx.FR_foot, 1, 1)[0][2] <= 0.05 else False)
-    b_RL_foot_contact = (True if pb.getLinkState(robot, Go2LinkIdx.RL_foot, 1, 1)[0][2] <= 0.05 else False)
-    b_RR_foot_contact = (True if pb.getLinkState(robot, Go2LinkIdx.RR_foot, 1, 1)[0][2] <= 0.05 else False)
-    
-    return (imu_frame_quat, imu_ang_vel, imu_dvel, joint_pos, joint_vel,
-            b_FL_foot_contact, b_FR_foot_contact, b_RL_foot_contact,
-            b_RR_foot_contact, FL_normal_force, FR_normal_force,
-            RL_normal_force, RR_normal_force)
+    b_FL_foot_contact = (
+        True
+        if pb.getLinkState(robot, Go2LinkIdx.FL_foot, 1, 1)[0][2] <= 0.05
+        else False
+    )
+    b_FR_foot_contact = (
+        True
+        if pb.getLinkState(robot, Go2LinkIdx.FR_foot, 1, 1)[0][2] <= 0.05
+        else False
+    )
+    b_RL_foot_contact = (
+        True
+        if pb.getLinkState(robot, Go2LinkIdx.RL_foot, 1, 1)[0][2] <= 0.05
+        else False
+    )
+    b_RR_foot_contact = (
+        True
+        if pb.getLinkState(robot, Go2LinkIdx.RR_foot, 1, 1)[0][2] <= 0.05
+        else False
+    )
+
+    return (
+        imu_frame_quat,
+        imu_ang_vel,
+        imu_dvel,
+        joint_pos,
+        joint_vel,
+        b_FL_foot_contact,
+        b_FR_foot_contact,
+        b_RL_foot_contact,
+        b_RR_foot_contact,
+        FL_normal_force,
+        FR_normal_force,
+        RL_normal_force,
+        RR_normal_force,
+    )
 
 
 def apply_control_input_to_pybullet(robot, command):
     mode = pb.TORQUE_CONTROL
 
-    # Front left 
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FL_hip_joint,
-                             controlMode=mode,
-                             force=command[0])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FL_thigh_joint,
-                             controlMode=mode,
-                             force=command[1])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FL_calf_joint,
-                             controlMode=mode,
-                             force=command[2])
-    # Front right 
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FR_hip_joint,
-                             controlMode=mode,
-                             force=command[3])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FR_thigh_joint,
-                             controlMode=mode,
-                             force=command[4])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.FR_calf_joint,
-                             controlMode=mode,
-                             force=command[5])
-    # Rear left 
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RL_hip_joint,
-                             controlMode=mode,
-                             force=command[6])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RL_thigh_joint,
-                             controlMode=mode,
-                             force=command[7])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RL_calf_joint,
-                             controlMode=mode,
-                             force=command[8])
-    # Rear right 
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RR_hip_joint,
-                             controlMode=mode,
-                             force=command[9])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RR_thigh_joint,
-                             controlMode=mode,
-                             force=command[10])
-    pb.setJointMotorControl2(robot,
-                             Go2JointIdx.RR_calf_joint,
-                             controlMode=mode,
-                             force=command[11])
+    # Front left
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FL_hip_joint, controlMode=mode, force=command[0]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FL_thigh_joint, controlMode=mode, force=command[1]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FL_calf_joint, controlMode=mode, force=command[2]
+    )
+    # Front right
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FR_hip_joint, controlMode=mode, force=command[3]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FR_thigh_joint, controlMode=mode, force=command[4]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.FR_calf_joint, controlMode=mode, force=command[5]
+    )
+    # Rear left
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RL_hip_joint, controlMode=mode, force=command[6]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RL_thigh_joint, controlMode=mode, force=command[7]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RL_calf_joint, controlMode=mode, force=command[8]
+    )
+    # Rear right
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RR_hip_joint, controlMode=mode, force=command[9]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RR_thigh_joint, controlMode=mode, force=command[10]
+    )
+    pb.setJointMotorControl2(
+        robot, Go2JointIdx.RR_calf_joint, controlMode=mode, force=command[11]
+    )
 
 
 def set_init_config_pybullet_robot(robot):
     knee_angle = 45
-    pb.resetJointState(robot, Go2JointIdx.FL_thigh_joint,
-                       np.radians(knee_angle), 0.0)
-    pb.resetJointState(robot, Go2JointIdx.FL_calf_joint,
-                       np.radians(-knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.FL_thigh_joint, np.radians(knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.FL_calf_joint, np.radians(-knee_angle), 0.0)
 
-    pb.resetJointState(robot, Go2JointIdx.FR_thigh_joint,
-                       np.radians(knee_angle), 0.0)
-    pb.resetJointState(robot, Go2JointIdx.FR_calf_joint,
-                       np.radians(-knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.FR_thigh_joint, np.radians(knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.FR_calf_joint, np.radians(-knee_angle), 0.0)
 
-    pb.resetJointState(robot, Go2JointIdx.RL_thigh_joint,
-                       np.radians(knee_angle), 0.0)
-    pb.resetJointState(robot, Go2JointIdx.RL_calf_joint,
-                       np.radians(-knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.RL_thigh_joint, np.radians(knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.RL_calf_joint, np.radians(-knee_angle), 0.0)
 
-    pb.resetJointState(robot, Go2JointIdx.RR_thigh_joint,
-                       np.radians(knee_angle), 0.0)
-    pb.resetJointState(robot, Go2JointIdx.RR_calf_joint,
-                       np.radians(-knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.RR_thigh_joint, np.radians(knee_angle), 0.0)
+    pb.resetJointState(robot, Go2JointIdx.RR_calf_joint, np.radians(-knee_angle), 0.0)
 
 
 def signal_handler(signal, frame):
@@ -188,9 +196,9 @@ def signal_handler(signal, frame):
         print("========================================================")
         print('saving list of compuation time in "compuation_time.txt"')
         print("========================================================")
-        np.savetxt("computation_time.txt",
-                   np.array([compuation_cal_list]),
-                   delimiter=",")
+        np.savetxt(
+            "computation_time.txt", np.array([compuation_cal_list]), delimiter=","
+        )
 
     if Config.VIDEO_RECORD:
         print("========================================================")
@@ -210,25 +218,29 @@ if __name__ == "__main__":
     pb.configureDebugVisualizer(pb.COV_ENABLE_GUI, 0)
 
     pb.resetDebugVisualizerCamera(
-        cameraDistance = 1.5,
-        cameraYaw = 120,
-        cameraPitch = -30,
-        cameraTargetPosition = [0, 0, 0.3],
+        cameraDistance=1.5,
+        cameraYaw=120,
+        cameraPitch=-30,
+        cameraTargetPosition=[0, 0, 0.3],
     )
     ## sim physics setting
-    pb.setPhysicsEngineParameter(fixedTimeStep=Config.CONTROLLER_DT,
-                                 numSubSteps=Config.N_SUBSTEP)
+    pb.setPhysicsEngineParameter(
+        fixedTimeStep=Config.CONTROLLER_DT, numSubSteps=Config.N_SUBSTEP
+    )
     pb.setGravity(0, 0, 0)
 
     ## robot spawn & initial kinematics and dynamics setting
     pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 0)
 
     # robot = pb.loadURDF(cwd + "/robot_model/go2/go2_description.urdf",
-                        # [0., 0., 0.45], [0, 0, 0, 1],
-                        # useFixedBase=False) 
-    robot = pb.loadURDF(cwd + "/robot_model/rad_arm.urdf",
-                        [0., 0., 0.45], [0, 0, 0, 1],
-                        useFixedBase=False) 
+    # [0., 0., 0.45], [0, 0, 0, 1],
+    # useFixedBase=False)
+    robot = pb.loadURDF(
+        cwd + "/robot_model/crab/crab.urdf",
+        [0.0, 0.0, 0.45],
+        [0, 0, 0, 1],
+        useFixedBase=False,
+    )
 
     # cylinder_robot = pb.loadURDF(
     #     cwd + "/robot_model/cylinder.urdf",
@@ -238,12 +250,12 @@ if __name__ == "__main__":
     # )
     cylinder_robot = pb.loadURDF(
         cwd + "/robot_model/cylinder.urdf",
-        [1., 1., 1.], [0, 0, 0.707, 0.707],
+        [1.0, 1.0, 1.0],
+        [0, 0, 0.707, 0.707],
         useFixedBase=False,
     )
 
-    ground = pb.loadURDF(cwd + "/robot_model/ground/plane.urdf",
-                         useFixedBase=1)
+    ground = pb.loadURDF(cwd + "/robot_model/ground/plane.urdf", useFixedBase=1)
     pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
     (
         n_q,
@@ -269,15 +281,15 @@ if __name__ == "__main__":
     # default robot kinematics information
     base_com_pos, base_com_quat = pb.getBasePositionAndOrientation(robot)
     rot_world_basecom = util.quat_to_rot(np.array(base_com_quat))
-    rot_world_basejoint = util.quat_to_rot(
-        np.array(Config.INITIAL_BASE_JOINT_QUAT))
+    rot_world_basejoint = util.quat_to_rot(np.array(Config.INITIAL_BASE_JOINT_QUAT))
 
     pos_basejoint_to_basecom = np.dot(
         rot_world_basejoint.transpose(),
         base_com_pos - np.array(Config.INITIAL_BASE_JOINT_POS),
     )
-    rot_basejoint_to_basecom = np.dot(rot_world_basejoint.transpose(),
-                                      rot_world_basecom)
+    rot_basejoint_to_basecom = np.dot(
+        rot_world_basejoint.transpose(), rot_world_basecom
+    )
 
     # TODO: pnc interface, sensor_data, command class
     rpc_go2_interface = go2_interface_py.Go2Interface()
@@ -319,15 +331,18 @@ if __name__ == "__main__":
         ##debugging state estimator by calculating groundtruth basejoint states
         base_com_pos, base_com_quat = pb.getBasePositionAndOrientation(robot)
         rot_world_basecom = util.quat_to_rot(base_com_quat)
-        rot_world_basejoint = np.dot(rot_world_basecom,
-                                     rot_basejoint_to_basecom.transpose())
-        base_joint_pos = base_com_pos - np.dot(rot_world_basejoint,
-                                               pos_basejoint_to_basecom)
+        rot_world_basejoint = np.dot(
+            rot_world_basecom, rot_basejoint_to_basecom.transpose()
+        )
+        base_joint_pos = base_com_pos - np.dot(
+            rot_world_basejoint, pos_basejoint_to_basecom
+        )
         base_joint_quat = util.rot_to_quat(rot_world_basejoint)
 
         base_com_lin_vel, base_com_ang_vel = pb.getBaseVelocity(robot)
-        trans_joint_com = liegroup.RpToTrans(rot_basejoint_to_basecom,
-                                             pos_basejoint_to_basecom)
+        trans_joint_com = liegroup.RpToTrans(
+            rot_basejoint_to_basecom, pos_basejoint_to_basecom
+        )
         adjoint_joint_com = liegroup.Adjoint(trans_joint_com)
         twist_basecom_in_world = np.zeros(6)
         twist_basecom_in_world[0:3] = base_com_ang_vel
@@ -335,15 +350,16 @@ if __name__ == "__main__":
         augrot_basecom_world = np.zeros((6, 6))
         augrot_basecom_world[0:3, 0:3] = rot_world_basecom.transpose()
         augrot_basecom_world[3:6, 3:6] = rot_world_basecom.transpose()
-        twist_basecom_in_basecom = np.dot(augrot_basecom_world,
-                                          twist_basecom_in_world)
-        twist_basejoint_in_basejoint = np.dot(adjoint_joint_com,
-                                              twist_basecom_in_basecom)
+        twist_basecom_in_basecom = np.dot(augrot_basecom_world, twist_basecom_in_world)
+        twist_basejoint_in_basejoint = np.dot(
+            adjoint_joint_com, twist_basecom_in_basecom
+        )
         augrot_world_basejoint = np.zeros((6, 6))
         augrot_world_basejoint[0:3, 0:3] = rot_world_basejoint
         augrot_world_basejoint[3:6, 3:6] = rot_world_basejoint
-        twist_basejoint_in_world = np.dot(augrot_world_basejoint,
-                                          twist_basejoint_in_basejoint)
+        twist_basejoint_in_world = np.dot(
+            augrot_world_basejoint, twist_basejoint_in_basejoint
+        )
         base_joint_ang_vel = twist_basejoint_in_world[0:3]
         base_joint_lin_vel = twist_basejoint_in_world[3:6]
 
@@ -363,10 +379,21 @@ if __name__ == "__main__":
         ############################################################
         # Get Sensor Data
         ############################################################
-        (imu_frame_quat, imu_ang_vel, imu_dvel, joint_pos, joint_vel,
-         b_FL_foot_contact, b_FR_foot_contact, b_RL_foot_contact,
-         b_RR_foot_contact, FL_normal_force, FR_normal_force, RL_normal_force,
-         RR_normal_force) = get_sensor_data_from_pybullet(robot)
+        (
+            imu_frame_quat,
+            imu_ang_vel,
+            imu_dvel,
+            joint_pos,
+            joint_vel,
+            b_FL_foot_contact,
+            b_FR_foot_contact,
+            b_RL_foot_contact,
+            b_RR_foot_contact,
+            FL_normal_force,
+            FR_normal_force,
+            RL_normal_force,
+            RR_normal_force,
+        ) = get_sensor_data_from_pybullet(robot)
 
         ## copy sensor data to rpc sensor data class
         rpc_go2_sensor_data.imu_frame_quat_ = imu_frame_quat
@@ -404,8 +431,7 @@ if __name__ == "__main__":
         apply_control_input_to_pybullet(robot, rpc_trq_command)
 
         # save current torso velocity for next iteration
-        previous_torso_velocity = pybullet_util.get_link_vel(
-            robot, Go2LinkIdx.imu)[3:6]
+        previous_torso_velocity = pybullet_util.get_link_vel(robot, Go2LinkIdx.imu)[3:6]
 
         ############################################################
         # Save Image file
@@ -413,7 +439,8 @@ if __name__ == "__main__":
         if (Config.VIDEO_RECORD) and (count % Config.RECORD_FREQ == 0):
             camera_data = pb.getDebugVisualizerCamera()
             frame = pybullet_util.get_camera_image_from_debug_camera(
-                camera_data, Config.RENDER_WIDTH, Config.RENDER_HEIGHT)
+                camera_data, Config.RENDER_WIDTH, Config.RENDER_HEIGHT
+            )
             filename = video_dir + "/step%06d.jpg" % jpg_count
             cv2.imwrite(filename, frame)
             jpg_count += 1
